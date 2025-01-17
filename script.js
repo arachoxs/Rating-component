0303785a32d7a050425f12c22997bc3a14ff47ae
arachox <j.agudelo9@utp.edu.co>
2025-01-16 22:46:34 -0500

    finished version

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const main=document.querySelector("main");
 const submit=document.querySelector("#submitMessage");
 const selectedStars = document.querySelector("#SelectedStars");
 
+const form = document.getElementById("form")
 
-button.onclick = update;
+button.addEventListener("click", update);
 
 function update(event){
     event.preventDefault();
     main.style.display="none";
+    const selectedRating = form.querySelector('input[name="star"]:checked');
     submit.style.display="flex";
+    selectedStars.innerHTML="You selected " + selectedRating.id + " out of 5";
 }
 
